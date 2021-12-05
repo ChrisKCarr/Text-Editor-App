420b6e97397bf03a03108b67f3eadef67e003ea4
Chris Carr <chriskcarrdev@gmail.com>
2021-12-05 10:49:07 +0100

    Creaated CLI commands

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server is listening');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('serving files to port: ', port);
+  console.log('saving/fetching cells from: ', filename);
+  console.log('that file can be found in: ', dir);
 };
